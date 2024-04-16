04b5a09fa688f104aa0e324e8b8c8757f760168b
h3ng3ll <h3ng3ll@gmail.com>
2024-04-16 22:02:54 +0300

    Unchecked changes cause late hour

--- src/models/CurrencyUAH.tsx
+++ src/models/CurrencyUAH.tsx
@@ -2,7 +2,7 @@ import { Double } from "react-native/Libraries/Types/CodegenTypes";
 import { Currency } from "./foundations/Currency";
 
 export class CurrencyUAH extends Currency {
-  convertCurrency(currency: Currency): Promise<Currency> {
+  async convertCurrency(currency: Currency): Promise<Currency> {
     throw new Error("Method not implemented.");
   }
 
